4f8e2468706dbc79168e3a1f94eb41a696fd790c
Derek Croote <dcroote@users.noreply.github.com>
2024-08-28 22:22:17 -0700

    fix: turn off meta padding rule and enable others individually

--- src/jest.js
+++ src/jest.js
@@ -20,7 +20,6 @@ module.exports = {
       rules: {
         'jest/max-expects': 'off', // Limiting expect statements is beneficial, but enforcing a strict count can be restrictive.
         'jest/no-hooks': 'off', // Would be time consuming to implement in existing repos.
-        'jest/padding-around-expect-groups': 'off', // Adds a lot of excess whitespace
         'jest/prefer-each': 'off', // We find traditional for-loops more readable in certain contexts.
         'jest/prefer-expect-assertions': 'off', // While useful, enforcing this can lead to verbose tests.
         'jest/prefer-importing-jest-globals': 'off', // This would be very bothersome for existing repos.
@@ -28,6 +27,15 @@ module.exports = {
         'jest/require-top-level-describe': 'off', // Multiple top-level describe blocks or tests can be acceptable.
         'jest/valid-title': 'off', // This restriction can prevent using titles like "<function-name>.name".
         'prefer-lowercase-title': 'off', // Sometimes we want to start the test with a capital letter and some words are all uppercase (e.g. AWS).,
+        // Padding rules have to be enabled individually if any are disabled
+        'jest/padding-around-all': 'off', // This meta rule needs to be off if any individual padding rules are off
+        'jest/padding-around-expect-groups': 'off', // Adds a lot of excess whitespace
+        'jest/padding-around-after-all-blocks': 'error',
+        'jest/padding-around-after-each-blocks': 'error',
+        'jest/padding-around-before-all-blocks': 'error',
+        'jest/padding-around-before-each-blocks': 'error',
+        'jest/padding-around-describe-blocks': 'error',
+        'jest/padding-around-test-blocks': 'error',
       },
     },
   ],
