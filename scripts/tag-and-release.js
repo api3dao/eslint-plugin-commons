d990b4f0b5427513b8821299f42150eea3c4080d
Derek Croote <dcroote@users.noreply.github.com>
2024-09-24 22:25:36 -0700

    feat: add tag-and-release workflow

---
+++ scripts/tag-and-release.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const { tagAndRelease } = require('@api3/commons');
+
+function main() {
+  return tagAndRelease('eslint-plugin-commons', path.join(__dirname, '../package.json'));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.info(error);
+    process.exitCode = 1;
+  });
